fae546370d5587a4ecd851fd64b055b4414ec883
xiajunwen1007 <49160447+xiajunwen1007@users.noreply.github.com>
2026-01-01 23:12:13 +0800

    Update Run_all.R

--- Run_all.R
+++ Run_all.R
@@ -2,11 +2,11 @@
 setwd("./Documents/quantile_optimal_regime/submission/")
 
 # source and run all result scripts
-# source("./code_result/Result_main_500.R")
+source("./code_result/Result_main_500.R")
 source("./code_result/Result_alter_smoothing.R")
-# source("./code_result/Result_inconsistency.R")
-# source("./code_result/Result_DR.R")
-# source("./code_result/Result_nonconvexity.R")
-# source("./code_result/Result_realdata.R")
-# source("./code_result/Result_smooth_survival.R")
-# source("./code_result/Result_main_diff_np.R")
+source("./code_result/Result_inconsistency.R")
+source("./code_result/Result_DR.R")
+source("./code_result/Result_nonconvexity.R")
+source("./code_result/Result_realdata.R")
+source("./code_result/Result_smooth_survival.R")
+source("./code_result/Result_main_diff_np.R")
